80bb2beafa03cec416c22d4a3d57ec43b902702b
Yharley Willy <112261759+YharleyWilly@users.noreply.github.com>
2022-08-27 21:11:50 -0300

    Exercício 003

---
+++ ex004.py
@@ -0,0 +1,23 @@
+#Exercício de classificar tipo primitivo do valor e identificar a variável.
+
+dgt = input('Digite algo: ')
+print('O tipo primitivo é: {}'.format(type(dgt)))
+print('Só tem espaços? {}'.format(dgt.isspace()))
+print('É alfabético? {}'.format(dgt.isalpha()))
+print('É um número? {}'.format(dgt.isnumeric()))
+print('Tem número e letra? {}'.format(dgt.isalnum()))
+print('É decimal?', dgt.isdecimal())
+print('Está em maiúsculo? {}'.format(dgt.isupper()))
+print('Está em minúsculo?', dgt.islower())
+
+
+
+
+
+
+
+
+
+
+
+
