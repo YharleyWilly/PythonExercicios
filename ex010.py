0cc14bbf420a60adf1fc8b68e1cf480855fedb9f
Yharley Willy <112261759+YharleyWilly@users.noreply.github.com>
2022-09-18 15:13:13 -0300

    Modificações

--- ex010.py
+++ ex010.py
@@ -1,3 +1,8 @@
 #Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos Dólares ela pode comprar.
-# Considere US$1.00=R$5.06
+#Considere US$1.00=R$5.06
+
+carteira = float(input("Digite a quantia disponível na sua carteira. USE PONTO (.) (##.###.##): "))
+dol = (carteira*1)/5.06
+print("Você pode comprar {} dólares".format(dol))
+
 
--- ex011.py
+++ ex011.py
@@ -1,2 +1,10 @@
 #Faça um programa que leia a largura e a altura de uma parede em metros, calcule sua área e a quatidade de tinta
 #necessária para pintá-la, sabendo que cada litro de tinta, pinta uma área de 2m**2
+
+larg = int(input("Digite a largura da parede (METROS): "))
+alt = int(input("Digite a altura da parede (METROS): "))
+area = larg*alt
+qntd_tinta = area/2
+print("Largura: {} \nAltura: {} \nÁrea: {} \nQuantidade de tinta necessária para pintá-la: {}"
+      .format(larg, alt, area, qntd_tinta))
+
